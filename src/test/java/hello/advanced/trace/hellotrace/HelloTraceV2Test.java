22cf5b46bedc10e96b06b59ca722aa65f54b699b
jungseonwoo <laborlawseon@gmail.com>
2022-12-31 16:51:12 +0900

    Initial commit

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,36 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class HelloTraceV2Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+    @Test
+    void stringAddSpaceMethod() {
+        String prefix = "-->";
+        int level = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append( (i == level - 1) ? "|" + prefix : "| ");
+        }
+        sb.toString();
+    }
+}
