3637c8a63852d7dc9f3eef9db068d11bd92af002
정선우 <laborlawseon@gmail.com>
2023-05-09 23:04:35 +0900

    Proxy Pattern 공부중

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,18 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
+/**
+ * scanBasePackages는 ComponentScan과 동일한 기능
+ * 기본은 main 메서드의 패키지와 하위 패키지를 componentScan한다.
+ */
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
