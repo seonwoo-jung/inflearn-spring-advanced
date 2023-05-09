25b95c14aaa7d20141c493a8b77bcb787e9d5c67
정선우 <laborlawseon@gmail.com>
2023-05-09 18:37:24 +0900

    - Module JDK, 의존성 설정 - proxy 프로젝트 Spring 버전 변경

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
-
 }
